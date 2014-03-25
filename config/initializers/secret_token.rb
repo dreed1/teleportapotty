323957957b88222447cb97766dd9b33f52bd6365
Dan Reed <danreedx86@gmail.com>
2014-03-24 23:08:26 -0700

    yep, i just bought teleportapotty.biz

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Teleportapotty::Application.config.secret_token = '91bc98c93ac20ea3acd86dbd4f1e9015f416d88137885c1faea9cbb7e108b21946ac2e063349f1bc6a42f98ceee4e4045590d45a5fd8fc7e21c244ed4d0559a6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Teleportapotty::Application.config.session_store :cookie_store, key: '_teleportapotty_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Teleportapotty::Application.config.session_store :active_record_store
